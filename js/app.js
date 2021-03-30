d6cd7801eaee9953278c180733c24778c8af918d
Barbara Engle <barbara@tshimologong.joburg>
2021-03-30 14:31:27 +0200

    First Draft of working code.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-const dogs [];
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const galleryImgs = document.querySelectorAll('.gallery-item');
+
+let currentlySelected = 0;
+
+prevBtn.addEventListener('click', function(){
+
+    galleryImgs[currentlySelected].classList.remove("active-item");
+
+    currentlySelected--;
+    galleryImgs[currentlySelected].classList.add("active-item");
+    nextBtn.disabled = false;
+
+    if (currentlySelected===0)
+    {
+        prevBtn.disabled = true;
+    }
+
+});
+
+nextBtn.addEventListener('click', function(){
+
+    galleryImgs[currentlySelected].classList.remove("active-item");
+    
+    currentlySelected++;
+    galleryImgs[currentlySelected].classList.add("active-item");
+
+    prevBtn.disabled=false;
+
+    if  (currentlySelected + 1 === galleryImgs.length)
+    {
+        nextBtn.disabled = true;
+    }
+});
